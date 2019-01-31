6fdce45c486fbd203da8e8217e6a4716fc004658
Sebastian Quintero <Sebastian Quintero@SEBASTIAN>
2019-01-31 16:13:33 -0500

    se realiza modificacion al codigo

--- src/anagram/Solution.java
+++ src/anagram/Solution.java
@@ -16,20 +16,20 @@ import java.util.ArrayList;
  */
 public class Solution {
 
-    ArrayList<String> anagram = new ArrayList<>();
-    ArrayList<String> resultado = new ArrayList<>();
+    ArrayList<String> lstAnagram = new ArrayList<>();
+    ArrayList<Character> palabraAnagram = new ArrayList<>();
+    ArrayList<Character> palabraCompar = new ArrayList<>();
 
     public void solutionProblem(String palabra) {
         subirArchivo(palabra);
     }
 //Metodo para cargar el archivo con las n palabras y para validar la palabra que ingreso el usuario con el archivo
+
     public void subirArchivo(String palabra) {
         File archivo = null;
         FileReader fr = null;
         BufferedReader br = null;
-        int contLetras = 0;
         boolean valido = true;
-        boolean conto = false;
 
         try {
             // Apertura del fichero y creacion de BufferedReader para poder
@@ -37,36 +37,18 @@ public class Solution {
             archivo = new File("D:\\anagram.txt");
             fr = new FileReader(archivo);
             br = new BufferedReader(fr);
-
             // Lectura del fichero
             String linea;
             while ((linea = br.readLine()) != null) {
-                valido = true;
-//          comprara tamaños de palabras y caracter por caracter
-                contLetras = 0;
                 if (palabra.length() == linea.length()) {
+                    limpiarListas();
                     for (int i = 0; i < palabra.length(); i++) {
-                        conto = false;
-                        valido = true;
-                        for (int j = 0; j < palabra.length(); j++) {
-                            if (valido == true) {
-                                if (palabra.charAt(i) == linea.charAt(j)) {//validacion de caracter  de la palabra con lista 
-                                    contLetras++;
-                                    conto = true;
-                                }
-                            }
-                            if (conto == true) {
-                                valido = false;
-                            }
-
-                        }
-
+                        palabraAnagram.add(palabra.charAt(i));
+                        palabraCompar.add(linea.charAt(i));
                     }
-                    if (contLetras == palabra.length()) {// añadimos la palabra que coincide con el numero de caracteres a la lista
-                        anagram.add(linea);
-
-                    }
+                    compararPalabras(linea);
                 }
+
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -82,50 +64,46 @@ public class Solution {
                 e2.printStackTrace();
             }
         }
-        validarPalabras(palabra);
+        imprimirAnagrama(palabra);
     }
-// metodo para la segunda validacion con las palabras que tenemos en el primer array list
-    public void validarPalabras(String palabra) {
-        int contLetras = 0;
-        boolean valido = true;
-        boolean conto = false;
-        for (int s = 0; s < anagram.size(); s++) {
-            contLetras = 0;
-            if (anagram.get(s).length() == palabra.length()) {
-                for (int i = 0; i < palabra.length(); i++) {
-                    conto = false;
-                    valido = true;
-                    for (int j = 0; j < palabra.length(); j++) {
-                        if (valido == true) {
-                            if (anagram.get(s).charAt(i) == palabra.charAt(j)) { //validacion de caracter de la posible lista con la palabra
-                                contLetras++;
-                                conto = true;
-                            }
-                        }
-                        if (conto == true) {
-                            valido = false;
-                        } else {
 
-                        }
-                    }
-
-                }
-                if (contLetras == palabra.length()) {
-                    resultado.add(anagram.get(s));
-                }
-
-            }
+    //limpiar listas
+    public void limpiarListas() {
+        for (int i = palabraAnagram.size() - 1; i >= 0; i--) {
+            palabraAnagram.remove(i);
+            palabraCompar.remove(i);
         }
 
-        imprimirPalabras(palabra);
-
     }
+    //-----------------------------------------------
+
+// metodo para validacion de las palabras
+    public void compararPalabras(String palAnagram) {
+        boolean ingresar = false;
+        int contLetras = 0;
+        boolean conto = false;
+        int total = palabraAnagram.size();
+        int i = 0;
+        for (int j = 0; j < palabraCompar.size(); j++) {
+            if (palabraAnagram.get(i) == palabraCompar.get(j)) {
+                contLetras++;
+                palabraAnagram.remove(i);
+                palabraCompar.remove(j);
+                j = -1;
+            }
+        }
+        i++;
+
+        if (contLetras == total) {
+            lstAnagram.add(palAnagram);
+        }
+    }
+
 //-------------------------------------------------------------
-    
 //  metodo para imprimir las palabras del anagram 
-    public void imprimirPalabras(String palabra) {
-        System.out.println("La palabra ingresada por el usuario es: " + palabra + "\n");
-        System.out.println("Las palabras ANAGRAMAS posibles son " + resultado);
+    public void imprimirAnagrama(String palabraAnagram) {
+        System.out.println("La palabra que usted ingreso es: " + palabraAnagram + "\n");
+        System.out.println("La lista de anagramas es la sigiente: " + lstAnagram);
     }
 //----------------------------------------------------    
 }
